881b2279cd5f8d919e9391932fd2217e789fe34c
Skwashlepro <skwashlepro@gmail.com>
2025-06-11 19:03:08 +0200

    invalid frees

--- srcs/utils/exits.c
+++ srcs/utils/exits.c
@@ -6,7 +6,7 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 02:52:45 by luctan            #+#    #+#             */
-/*   Updated: 2025/06/11 18:48:32 by luctan           ###   ########.fr       */
+/*   Updated: 2025/06/11 19:02:56 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 void	ft_kill(t_data *data, int code)
 {
 	free_array(data->map);
-	mlx_destroy_image(data->display.mlx, data->frame.img);
 	mlx_destroy_display(data->display.mlx);
 	free(data->display.mlx);
-	free(data);
 	exit(code);
 }
 
