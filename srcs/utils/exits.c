56e8c793611a431442f858c67ee2a96db9e1edf3
Skwashlepro <skwashlepro@gmail.com>
2025-06-13 02:00:19 +0200

    if statement in ft_kill

--- srcs/utils/exits.c
+++ srcs/utils/exits.c
@@ -6,7 +6,7 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 02:52:45 by luctan            #+#    #+#             */
-/*   Updated: 2025/06/13 01:55:59 by luctan           ###   ########.fr       */
+/*   Updated: 2025/06/13 01:59:59 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 void	ft_kill(t_data *data, int code)
 {
 	clear_db(data);
-	mlx_destroy_window(data->display.mlx, data->display.mlx_win);
-	mlx_destroy_display(data->display.mlx);
-	free(data->display.mlx);
+	if (data->display.mlx_win && data->display.mlx)
+	{
+		mlx_destroy_window(data->display.mlx, data->display.mlx_win);
+		mlx_destroy_display(data->display.mlx);
+		free(data->display.mlx);
+	}
 	exit(code);
 }
 
