553b1e3f4c61fa0a633d8dcccd5cbc0d53d2cf5d
Skwashlepro <skwashlepro@gmail.com>
2025-06-26 15:09:40 +0200

    no leaks movements added

--- srcs/input_handling/keys.c
+++ srcs/input_handling/keys.c
@@ -6,7 +6,7 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 21:56:50 by luctan            #+#    #+#             */
-/*   Updated: 2025/06/23 15:37:46 by luctan           ###   ########.fr       */
+/*   Updated: 2025/06/26 15:08:11 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,11 @@ int	key_listener(int keycode, t_data *data)
 {
 	if (keycode == ESC_KEY)
 		ft_kill(data, 0);
-	// else if (ft_strchr("WASD", keycode))
-	// 	movements(keycode, data);
 	else if (keycode == LEFT_KEY)
 		rot_cam(data, -ROT_SPEED);
 	else if (keycode == RIGHT_KEY)
 		rot_cam(data, ROT_SPEED);
+	else
+		movements(keycode, data);
 	return (0);
 }
-
-
-
--- srcs/input_handling/movements.c
+++ srcs/input_handling/movements.c
@@ -6,15 +6,63 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 02:21:46 by luctan            #+#    #+#             */
-/*   Updated: 2025/06/23 15:27:34 by luctan           ###   ########.fr       */
+/*   Updated: 2025/06/26 15:09:09 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	fwd(t_data *data);
+void	fwd(t_data *data)
+{
+	double	new_x;
+	double	new_y;
 
-void	back(t_data *data);
+	new_x = data->p1.pos_x + data->p1.dir_x * 0.1;
+	new_y = data->p1.pos_y + data->p1.dir_y * 0.1;
+	if (data->map[(int)new_y][(int)new_x] != '1')
+	{
+		data->p1.pos_x = new_x;
+		data->p1.pos_y = new_y;
+	}
+}
+
+void	back(t_data *data)
+{
+	double	new_x;
+	double	new_y;
+
+	new_x = data->p1.pos_x - data->p1.dir_x * 0.1;
+	new_y = data->p1.pos_y - data->p1.dir_y * 0.1;
+	if (data->map[(int)new_y][(int)new_x] != '1')
+	{
+		data->p1.pos_x = new_x;
+		data->p1.pos_y = new_y;
+	}
+}
+
+void	side(t_data *data, int keycode)
+{
+	double	new_x;
+	double	new_y;
+
+	new_x = data->p1.pos_x;
+	new_y = data->p1.pos_y;
+	if (keycode == A_KEY)
+	{
+		new_x = data->p1.pos_x - data->p1.plane_x * 0.1;
+		new_y = data->p1.pos_y - data->p1.plane_y * 0.1;
+	}
+	else if (keycode == D_KEY)
+	{
+		new_x = data->p1.pos_x + data->p1.plane_x * 0.1;
+		new_y = data->p1.pos_y + data->p1.plane_y * 0.1;
+	}
+	if (data->map[(int)new_y][(int)new_x] != '1')
+	{
+		data->p1.pos_x = new_x;
+		data->p1.pos_y = new_y;
+	}
+}
 
 void	movements(int keycode, t_data *data)
 {
