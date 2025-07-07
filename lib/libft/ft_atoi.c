08e9c3eb5b9ed6c9d1aaeadefe5c756bb6466ef6
skwashlepro <skwashlepro@gmail.com>
2025-07-07 18:13:52 +0200

    fixed caca

--- lib/libft/ft_atoi.c
+++ lib/libft/ft_atoi.c
@@ -6,11 +6,12 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:09:10 by luctan            #+#    #+#             */
-/*   Updated: 2023/11/15 19:45:24 by luctan           ###   ########.fr       */
+/*   Updated: 2025/07/07 18:13:26 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int	ft_atoi(const char *nptr)
 {
@@ -23,6 +24,8 @@ int	ft_atoi(const char *nptr)
 	r = 0;
 	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
+	if (!nptr[i])
+		return (-1);
 	if (nptr[i] == '+' || nptr[i] == '-')
 	{
 		if (nptr[i] == '-')
